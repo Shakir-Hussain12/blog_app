acf31bc69cb4712afa0e2362c4e9591f20294742
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-05 09:25:23 +0500

    Added index to authod_id in comments & posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_034905) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_05_042249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_034905) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "post_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -40,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_034905) do
     t.integer "likes_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
