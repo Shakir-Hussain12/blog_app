8ca52f8debae736747238e082d00d6461655c64d
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-05 09:10:11 +0500

    Fixed author_id type with CAST

--- db/migrate/20230505034905_change_author_id_type_and_add_foreign_key_to_comments.rb
+++ db/migrate/20230505034905_change_author_id_type_and_add_foreign_key_to_comments.rb
@@ -1,6 +1,6 @@
 class ChangeAuthorIdTypeAndAddForeignKeyToComments < ActiveRecord::Migration[7.0]
   def change
-    change_column :comments, :author_id, :integer
+    change_column :comments, :author_id, 'integer USING author_id::integer'
     add_foreign_key :comments, :users, column: :author_id
   end
 end
