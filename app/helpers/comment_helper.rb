d19ef05bcb5ee78e8569494c45f9e54443851a5e
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-13 15:10:00 +0500

    Added helper for comments

---
+++ app/helpers/comment_helper.rb
@@ -0,0 +1,17 @@
+module CommentHelper
+    def show_comments(post)
+      comments = Comment.where(post_id: post.id)
+  
+      if post.comments_count.nil?
+        content_tag(:p, "No Comments Yet!")
+      else
+        output = content_tag(:h3, "Post Comments")
+        comments.each do |comment|
+          user = User.find(comment.author_id)
+          comment_html = content_tag(:p, "#{user.name}: #{comment.text}")
+          output += content_tag(:li, comment_html, class: "comment-item")
+        end
+        output
+      end
+    end
+  end
