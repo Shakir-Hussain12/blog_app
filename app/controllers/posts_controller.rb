ba9e4af9ca8f4f5025ad303f3071419780fb9220
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-13 18:46:16 +0500

    Added pagination for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: @user.id)
+    @posts = Post.where(author_id: @user.id).paginate(page: params[:page], per_page: 2)
     render 'index', locals: { user: @user, posts: @posts }
   end
 
