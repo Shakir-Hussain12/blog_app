055f88d1b008b18c8d58ef072f6e01fbc9a88f8c
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-05-20 19:50:03 +0500

    Added Bio, Name, and Photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { request.format.json? }
 
   include Devise::Controllers::Helpers
+  before_action :allowed_parameters, if: :devise_controller?
   before_action :authenticate_user!, unless: -> { request.format.json? }
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password])
+  def allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :photo, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :current_password)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  after_initialize :set_name
-
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id', through: :posts
   has_many :likes, foreign_key: 'author_id', through: :posts
 
   validates :name, presence: true
   validates :posts_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-
-  def set_name
-    self.name ||= email.split('@').first || 'Anonymous'
-  end
 
   recent_posts = ->(no) { posts.order(created_at: :desc).limit(no) }
   define_method :recent_posts, recent_posts
